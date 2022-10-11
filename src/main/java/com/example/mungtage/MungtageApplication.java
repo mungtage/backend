b017a737cbda728b1d5fb998495cb38586f4f29b
jhdl0157 <jhdl0157@naver.com>
2022-10-11 15:37:53 +0700

    프로젝트 세팅

---
+++ src/main/java/com/example/mungtage/MungtageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mungtage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungtageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MungtageApplication.class, args);
+    }
+
+}
