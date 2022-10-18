51f8faadeb09527070f4d27130b3b00546bab13d
jhdl0157 <jhdl0157@naver.com>
2022-10-18 16:04:59 +0700

    Feat : jwt 토큰 추가

--- src/main/java/com/example/mungtage/domain/Controller.java
+++ src/main/java/com/example/mungtage/domain/Controller.java
@@ -1,5 +1,7 @@
 package com.example.mungtage.domain;
 
+import com.example.mungtage.domain.User.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
