d27489b3b7c74fde5157c418d789cd2e8efa7053
newbiejanghan <bjh4135@gmail.com>
2022-10-26 19:22:55 +0900

    AI server api request

--- src/main/java/com/example/mungtage/domain/Match/MatchService.java
+++ src/main/java/com/example/mungtage/domain/Match/MatchService.java
@@ -8,9 +8,15 @@ import com.example.mungtage.domain.Match.dto.MatchTrialDto;
 import com.example.mungtage.domain.Match.dto.MatchResultDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -63,5 +69,20 @@ public class MatchService {
 
     public MatchTrial getMatchTrial(Long matchTrialId) throws ChangeSetPersister.NotFoundException{
         return matchTrialRepository.findById(matchTrialId).orElseThrow(ChangeSetPersister.NotFoundException::new);
+    }
+
+    public Object requestToAIServer(String imageUrl) throws URISyntaxException, HttpServerErrorException {
+        RestTemplate restTemplate = new RestTemplate();
+        String aiServerURL = "http://49.50.163.148:5000/pd/?img=";
+
+        Object response = restTemplate.getForObject(new URI(aiServerURL+"1"), Object.class);
+        System.out.println(response);
+
+        return response;
+//        if (response.getStatusCode() == HttpStatus.OK) {
+//            return response;
+//        } else {
+//            throw new HttpServerErrorException(response.getStatusCode(), "AI server request error");
+//        }
     }
 }
