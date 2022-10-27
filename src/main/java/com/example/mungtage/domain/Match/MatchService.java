8093a0468c70e5cd50551e75ce3fc6d7b5e8de31
jhdl0157 <jhdl0157@naver.com>
2022-10-27 22:50:09 +0900

    Feat : Json to Map 추가

--- src/main/java/com/example/mungtage/domain/Match/MatchService.java
+++ src/main/java/com/example/mungtage/domain/Match/MatchService.java
@@ -6,23 +6,31 @@ import com.example.mungtage.domain.Match.model.MatchResult;
 import com.example.mungtage.domain.Match.model.MatchTrial;
 import com.example.mungtage.domain.Match.dto.MatchTrialDto;
 import com.example.mungtage.domain.Match.dto.MatchResultDto;
+import com.example.mungtage.util.exception.BadRequestException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MatchService {
+    private final ObjectMapper objectMapper;
     private final MatchTrialRepository matchTrialRepository;
     private final MatchResultRepository matchResultRepository;
     private final LostRepository lostRepository;
@@ -71,9 +79,8 @@ public class MatchService {
         return matchTrialRepository.findById(matchTrialId).orElseThrow(ChangeSetPersister.NotFoundException::new);
     }
 
-    public Object requestToAIServer(String imageUrl) throws URISyntaxException, HttpServerErrorException {
+    public Map<String,String> requestToAIServer(String imageUrl) throws URISyntaxException, HttpServerErrorException {
         RestTemplate restTemplate = new RestTemplate();
-
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
@@ -81,14 +88,18 @@ public class MatchService {
         System.out.println(entity);
         String aiServerURL = "http://49.50.163.148:5000/pd/?img=";
 
-        ResponseEntity<Object> response = restTemplate.exchange(new URI(aiServerURL+imageUrl), HttpMethod.GET, entity, Object.class);
-        System.out.println(response);
-
-        return response;
-//        if (response.getStatusCode() == HttpStatus.OK) {
-//            return response;
-//        } else {
-//            throw new HttpServerErrorException(response.getStatusCode(), "AI server request error");
-//        }
+        ResponseEntity<String> response = restTemplate.exchange(new URI(aiServerURL+imageUrl), HttpMethod.GET, entity, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            log.info("응답에 성공!! {}",response.getBody());
+            try {
+                Map<String, String> map = objectMapper.readValue(response.getBody(), Map.class);
+                log.info("Json to Map 변환 성공!! {}",map);
+                return map;
+            } catch (IOException e) {
+                throw new BadRequestException("변환 실패!!!");
+            }
+        } else {
+            throw new HttpServerErrorException(response.getStatusCode(), "AI server request error");
+        }
     }
 }
